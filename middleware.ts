da56c5d6748080245bc5c79bbc3d8ad057de8097
Hassan <hassankamran508@gmail.com>
2026-06-01 23:53:28 +0500

    Nexus v1 completed

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,14 @@
-export { default } from "next-auth/middleware"
+import { withAuth } from "next-auth/middleware"
+
+export default withAuth(
+  function middleware(req) {
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+)
 
 export const config = { 
   matcher: ["/dashboard/:path*", "/chat/:path*"] 
